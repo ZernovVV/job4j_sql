877407a6279d2460753702101b2eac8b1c9b7fb0
ZernovVV <vorksha@gmail.com>
2026-07-20 23:19:58 +0300

    2.3.3.3. Ограничения целостности [#505602]

---
+++ 505602_ограничение_целостности/tracker.sql
@@ -0,0 +1,46 @@
+CREATE TABLE roles
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name          TEXT NOT NULL CHECK (char_length (name) > 0)
+);
+
+CREATE TABLE users
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    login         TEXT NOT NULL UNIQUE,
+    email         TEXT NOT NULL UNIQUE,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    password_hash TEXT NOT NULL CHECK (char_length (password_hash) > 0),
+    active        BOOLEAN NOT NULL DEFAULT true,
+    role_id       BIGINT NOT NULL REFERENCES roles(id)
+);
+
+CREATE TABLE states
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name          TEXT NOT NULL CHECK (char_length (name) > 0)
+);
+
+CREATE TABLE categories
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name          TEXT NOT NULL CHECK (char_length (name) > 0)
+);
+
+CREATE TABLE items
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title         TEXT NOT NULL CHECK (char_length (title) > 0),
+    discription   TEXT NOT NULL CHECK (char_length (discription) > 0),
+    author_id     BIGINT NOT NULL REFERENCES users(id),
+    state_id      BIGINT NOT NULL REFERENCES states(id),
+    category_id   BIGINT NOT NULL REFERENCES categories(id)
+);
+
+CREATE TABLE comments
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    comment       TEXT NOT NULL CHECK (char_length (comment) > 0),
+    author_id     BIGINT NOT NULL REFERENCES users(id),
+    item_id       BIGINT NOT NULL REFERENCES items(id)
+);
