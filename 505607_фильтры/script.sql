5c76ad5fe289a2e86ef5d77dc7e46afe689ce704
ZernovVV <vorksha@gmail.com>
2026-07-27 23:42:17 +0300

    2.3.5.2. Фильтры [#505607]

---
+++ 505607_фильтры/script.sql
@@ -0,0 +1,51 @@
+SELECT id, name, price, is_active
+FROM products
+WHERE is_active = true;
+
+
+SELECT id, name, price
+FROM products
+WHERE price BETWEEN 10000 AND 100000;
+
+
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE status = 'NEW' OR status = 'PAID';
+
+
+INSERT INTO orders (user_id, status)
+VALUES
+    (1, 'CANCELLED'),
+    (1, 'CANCELLED'),
+    (1, 'CANCELLED'),
+    (1, 'CANCELLED');
+
+
+INSERT INTO order_items (order_id, product_id, quantity, unit_price)
+VALUES
+    (3, 1, 1, 99990.00),
+    (4, 1, 2, 99990.00),
+    (5, 1, 3, 99990.00),
+    (6, 1, 4, 99990.00);
+
+
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE user_id = 1 AND status <> 'CANCELLED';
+
+
+SELECT id, name, email, created_at
+FROM users
+WHERE created_at BETWEEN
+      TIMESTAMPTZ '2026-01-01 00:00:00+00'
+  AND TIMESTAMPTZ '2026-12-31 23:59:59+00';
+
+
+SELECT id, name, price
+FROM products
+WHERE price < 20000 OR price > 80000;
+
+SELECT id, name, price
+FROM products
+WHERE is_active = true
+  AND price < 3000 OR price > 150000;
